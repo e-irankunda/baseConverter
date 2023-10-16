10234ae391b9dc1faf916453ce70eebb18f6bec5
Eliphase Irankunda <116687966+e-irankunda@users.noreply.github.com>
2023-10-16 03:03:29 +0200

    Update and rename baseConverter-1.ipynb to baseConverter-v1.py

---
+++ baseConverter-v1.py
@@ -0,0 +1,17 @@
+userNum = int(input('Enter the number to convert: '))
+base = int(input('Enter the base: '))
+n = userNum
+
+list_digit = []
+while n!= 1:
+    k = n % base 
+    list_digit.append(k)
+    n = n // base
+list_digit.append(n)
+list_digit.reverse()
+
+newNum = ''
+for i in list_digit:
+    newNum += str(i)
+
+print(f'The (decimal) {userNum} converts into {newNum} in base {base}')
